b879da6c5f276c69dd0bae04f349638dc6a3435f
pedrofardin <pedro.fardin@hotmail.com>
2025-03-26 10:37:56 -0300

    api

---
+++ src/test/java/br/insper/cursos/service/CursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.cursos.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
